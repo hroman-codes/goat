b74a741229b3e9b66be56978648c639c4b4da3ba
getromandev <getroman.dev@gmail.com>
2020-01-15 16:17:22 -0500

    write routes to components

--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from './landingpage';
+import Resume from './resume';
+import Project from './projects';
+import Contact from './contact';
+import About from './aboutme';
 
 const Main = () => (
     <Switch>
         <Route exact path='/' component={LandingPage} />
+        <Route path='/resume' component={Resume} />
+        <Route path='/projects' component={Project} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/aboutme' component={About} />
     </Switch>
 )
 
