7a865091854d2d2bd1de5c37602a131c3fe50fec
getromandev <getroman.dev@gmail.com>
2020-01-16 18:55:21 -0500

    add banner text and social icons

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to the landing page</h1>
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Grid className='landingGrid'>
+                    <Cell col={12}>
+                        <img 
+                            src='https://cdn.clipart.email/c3354c76b84aaefe770c73447226b77c_clipart-cute-robot-head_2400-2400.png'
+                            alt='getroman logo'
+                            className='getromanLogo' 
+                        />
+
+                        <div className='bannerText'>
+                            <h1>Lead Technical Instructor | Dev | AWS Certified Cloud Practitioner</h1>
+                        <hr/>
+
+                        <p>HTML | CSS | Javascript | Design | Express | SQL | MongoDB | React | Redux | React-Native</p>
+
+                        <div className='socialLinks'>
+
+                            {/* linkedin */}
+                            <a href='https://www.linkedin.com/in/heribertoroman/' rel='noopener noreferrer' target='_blank'>
+                                <i class="fab fa-linkedin-in fa-2x"></i>
+                            </a>
+
+                            {/* github */}
+                            <a href='https://github.com/getromandev' rel='noopener noreferrer' target='_blank'>
+                                <i class="fab fa-github fa-2x"></i>
+                            </a>
+
+                             {/* AWS Cert */}
+                            <a href='https://www.certmetrics.com/amazon/public/badge.aspx?i=9&t=c&d=2019-12-02&ci=AWS01186088' rel='noopener noreferrer' target='_blank'>
+                                <i class="fab fa-aws fa-2x"></i>
+                            </a>
+
+                            {/* medium */}
+                            <a href='https://medium.com/@getroman.dev' rel='noopener noreferrer' target='_blank'>
+                                <i class="fab fa-medium-m fa-2x"></i>
+                            </a>
+
+                            {/* dribble */}
+                            <a href='https://dribbble.com/getroman' rel='noopener noreferrer' target='_blank'>
+                                <i class="fab fa-dribbble fa-2x"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
