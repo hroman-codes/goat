7409be3dd2b025b58917e2a3cbe5ca63009f5140
getromandev <getroman.dev@gmail.com>
2020-01-19 15:45:13 -0500

    add grid and cell to resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return (
             <div>
-                <h1>Resume Page</h1>
+                <Grid>
+                    <Cell col={4}>Left Side</Cell>
+                    <Cell col={8} className='resumeRightCol'>Right Side</Cell>
+                </Grid>
             </div>
         )
     }
