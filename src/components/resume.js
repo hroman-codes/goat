c70f9630fb0de8d7a15db6af62b8fb378f9bf4a9
getromandev <getroman.dev@gmail.com>
2020-01-19 23:28:02 -0500

    create experience component and pass props to resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Education from './education';
+import Experience from './experience';
 
 class Resume extends Component {
     render() {
@@ -67,10 +68,25 @@ class Resume extends Component {
                             schoolName='My University'
                             schoolDescription='Something goes here'
                         />
-
                         <hr style={{ borderTop: '3px solid #e22947 '}}/>
 
                         <h2>Experience</h2>
+                        <Experience 
+                            startYear={2009}
+                            endYear={2012}
+                            jobName='Job 2'
+                            jobDescription='yo yo yo yo'
+                        />
+
+                        <Experience 
+                            startYear={2009}
+                            endYear={2012}
+                            jobName='Job 1'
+                            jobDescription='yo yo yo yo'
+                        />
+                        <hr style={{ borderTop: '3px solid #e22947 '}}/>
+
+                        <h2>Skills</h2>
                     </Cell>
                 </Grid>
             </div>
