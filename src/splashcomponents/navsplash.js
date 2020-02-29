b1e66292f5b11b286de3dd4f584cca1a25fc6a45
getromandev <getroman.dev@gmail.com>
2020-02-28 21:25:14 -0500

    add grid to nav area and add logoto center of page

--- src/splashcomponents/navsplash.js
+++ src/splashcomponents/navsplash.js
@@ -15,10 +15,27 @@ class Navsplash extends Component {
     render() {
         return (
             <div className='cloudContainer'>
-
                 <NavHeader />
-
-                <SplashGetromanLogo />
+                
+                <div className='spaceContainer'>
+                    {/* <div className='splashGetromanLogo'>
+                        <SplashGetromanLogo />
+                    </div> */}
+                    <div className='item1'></div>
+                    <div className='item2'></div>
+                    <div className='item3'></div>
+                    <div className='item4'><SplashGetromanLogo /></div>
+                    <div className='item5'></div>
+                    <div className='item6'></div>
+                    <div className='item7'></div>
+                    {/* <div className='item8'></div>
+                    <div className='item9'></div>
+                    <div className='item10'></div>
+                    <div className='item11'></div>
+                    <div className='item12'></div> */}
+                    {/* <div className='item2'></div>
+                    <div className='item3'></div> */}
+                </div>
   
                 <div className='footer'>
                     <Cloudsplash />
